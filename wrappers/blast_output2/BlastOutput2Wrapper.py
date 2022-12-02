6a8b80c4066bba9619359830d8bff6002e40af76
Philip Johnson <p.zhao.johnson@gmail.com>
2022-12-02 12:32:52 -0500

    Add reports getter to BlastOutput2Wrapper class

--- wrappers/blast_output2/BlastOutput2Wrapper.py
+++ wrappers/blast_output2/BlastOutput2Wrapper.py
@@ -1,6 +1,13 @@
+from wrappers.report.ReportWrapper import ReportWrapper
+
+
 class BlastOutput2Wrapper:
     def __init__(self, blast_output2):
         if 'BlastOutput2' in blast_output2:
             self.blast_output2 = blast_output2['BlastOutput2']
         else:
             self.blast_output2 = blast_output2
+
+    @property
+    def reports(self):
+        return [ReportWrapper(report) for report in self.blast_output2]
--- wrappers/blast_output2/test_BlastOutput2Wrapper.py
+++ wrappers/blast_output2/test_BlastOutput2Wrapper.py
@@ -13,6 +13,9 @@ with open(examples_directory_path + 'unkeyed.json', 'r') as f:
 with open(examples_directory_path + 'keyed.json', 'r') as f:
     keyed = json.loads(f.read())
 
+with open(examples_directory_path + 'zero_reports.json', 'r') as f:
+    zero_reports = json.loads(f.read())
+
 
 class TestBlastOutput2Property(unittest.TestCase):
     def test_unkeyed(self):
@@ -22,3 +25,34 @@ class TestBlastOutput2Property(unittest.TestCase):
     def test_keyed(self):
         blast_output2 = BlastOutput2Wrapper(keyed)
         self.assertIs(blast_output2.blast_output2, keyed['BlastOutput2'])
+
+
+class TestReportsGetter(unittest.TestCase):
+    def test_unkeyed(self):
+        blast_output2 = BlastOutput2Wrapper(unkeyed)
+        reports = blast_output2.reports
+        self.assertEqual(len(reports), 2)
+        report1 = reports[0]
+        report2 = reports[1]
+        # just check some report values
+        query_from1 = report1.results.search.hits[0].hsps[0].query_from
+        self.assertEqual(query_from1, 561)
+        hit_from2 = report2.results.search.hits[0].hsps[0].hit_from
+        self.assertEqual(hit_from2, 790)
+
+    def test_keyed(self):
+        blast_output2 = BlastOutput2Wrapper(keyed)
+        reports = blast_output2.reports
+        self.assertEqual(len(reports), 2)
+        report1 = reports[0]
+        report2 = reports[1]
+        # just check some report values
+        query_to1 = report1.results.search.hits[0].hsps[0].query_to
+        self.assertEqual(query_to1, 2013)
+        hit_from2 = report2.results.search.hits[0].hsps[0].hit_from
+        self.assertEqual(hit_from2, 1223)
+
+    def test_zero_reports(self):
+        blast_output2 = BlastOutput2Wrapper(zero_reports)
+        reports = blast_output2.reports
+        self.assertEqual(len(reports), 0)
