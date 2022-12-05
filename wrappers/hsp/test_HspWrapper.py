a19706d42f5d12bbaeeb57c09544813d776f6921
Philip Johnson <p.zhao.johnson@gmail.com>
2022-12-05 09:53:24 -0500

    Split HspWrapper class tests into multiple test case classes

--- wrappers/hsp/test_HspWrapper.py
+++ wrappers/hsp/test_HspWrapper.py
@@ -9,13 +9,43 @@ with open('wrappers/hsp/example_hsps/hsp1.json', 'r') as f:
     hsp1 = json.loads(f.read())
 
 
-class TestHspWrapper(unittest.TestCase):
+class TestHspProperty(unittest.TestCase):
     def test_hsp1(self):
         hsp = HspWrapper(hsp1)
         self.assertIs(hsp.hsp, hsp1)
+
+
+class TestQueryFromGetter(unittest.TestCase):
+    def test_hsp1(self):
+        hsp = HspWrapper(hsp1)
         self.assertEqual(hsp.query_from, 561)
+
+
+class TestQueryToGetter(unittest.TestCase):
+    def test_hsp1(self):
+        hsp = HspWrapper(hsp1)
         self.assertEqual(hsp.query_to, 1234)
+
+
+class TestQueryStrandGetter(unittest.TestCase):
+    def test_hsp1(self):
+        hsp = HspWrapper(hsp1)
         self.assertEqual(hsp.query_strand, 'Plus')
+
+
+class TestHitFromGetter(unittest.TestCase):
+    def test_hsp1(self):
+        hsp = HspWrapper(hsp1)
         self.assertEqual(hsp.hit_from, 683)
+
+
+class TestHitToGetter(unittest.TestCase):
+    def test_hsp1(self):
+        hsp = HspWrapper(hsp1)
         self.assertEqual(hsp.hit_to, 1)
+
+
+class TestHitStrandGetter(unittest.TestCase):
+    def test_hsp1(self):
+        hsp = HspWrapper(hsp1)
         self.assertEqual(hsp.hit_strand, 'Minus')
