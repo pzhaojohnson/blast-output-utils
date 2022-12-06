aeeddccc4b7530cbc49838fe0f29ff0fd90d598f
Philip Johnson <p.zhao.johnson@gmail.com>
2022-12-06 10:35:45 -0500

    Add hsps_sorted_by_hit_from getter to HitWrapper class

--- wrappers/hit/HitWrapper.py
+++ wrappers/hit/HitWrapper.py
@@ -1,5 +1,7 @@
 from wrappers.hsp.HspWrapper import HspWrapper
 
+import operator
+
 
 class HitWrapper:
     def __init__(self, hit):
@@ -8,3 +10,9 @@ class HitWrapper:
     @property
     def hsps(self):
         return [HspWrapper(hsp) for hsp in self.hit['hsps']]
+
+    @property
+    def hsps_sorted_by_hit_from(self):
+        hsps = self.hsps
+        hsps.sort(key=operator.attrgetter('hit_from'))
+        return hsps
--- wrappers/hit/test_HitWrapper.py
+++ wrappers/hit/test_HitWrapper.py
@@ -11,6 +11,9 @@ with open('wrappers/hit/example_hits/zero_hsps.json', 'r') as f:
 with open('wrappers/hit/example_hits/two_hsps.json', 'r') as f:
     two_hsps = json.loads(f.read())
 
+with open('wrappers/hit/example_hits/four_hsps.json', 'r') as f:
+    four_hsps = json.loads(f.read())
+
 
 class TestHitProperty(unittest.TestCase):
     def test_zero_hsps(self):
@@ -31,3 +34,20 @@ class TestHspsGetter(unittest.TestCase):
         # check just some hsp values
         self.assertEqual(hsp1.query_to, 1008)
         self.assertEqual(hsp2.hit_from, 2678)
+
+
+class TestHspsSortedByHitFromGetter(unittest.TestCase):
+    def test_zero_hsps(self):
+        hit = HitWrapper(zero_hsps)
+        self.assertEqual(len(hit.hsps_sorted_by_hit_from), 0)
+
+    def test_four_hsps(self):
+        hit = HitWrapper(four_hsps)
+        hsps_sorted_by_hit_from = hit.hsps_sorted_by_hit_from
+        # check that hsps are not already sorted
+        self.assertNotEqual(hsps_sorted_by_hit_from, hit.hsps)
+        self.assertEqual(len(hsps_sorted_by_hit_from), 4)
+        self.assertEqual(hsps_sorted_by_hit_from[0].hit_from, 1223)
+        self.assertEqual(hsps_sorted_by_hit_from[1].hit_from, 1366)
+        self.assertEqual(hsps_sorted_by_hit_from[2].hit_from, 2366)
+        self.assertEqual(hsps_sorted_by_hit_from[3].hit_from, 2371)
