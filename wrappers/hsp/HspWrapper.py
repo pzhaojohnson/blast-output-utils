73f3a3677f50ad82e2da96ded80025727bd936d8
Philip Johnson <p.zhao.johnson@gmail.com>
2023-05-16 13:50:16 -0400

    Use wrappee getter in place of hsp property

--- wrappers/hsp/HspWrapper.py
+++ wrappers/hsp/HspWrapper.py
@@ -9,19 +9,19 @@ class HspWrapper:
 
     @property
     def query_from(self):
-        query_from = self.hsp['query_from']
+        query_from = self.wrappee['query_from']
         assert type(query_from) is int
         return query_from
 
     @property
     def query_to(self):
-        query_to = self.hsp['query_to']
+        query_to = self.wrappee['query_to']
         assert type(query_to) is int
         return query_to
 
     @property
     def query_strand(self):
-        query_strand = self.hsp['query_strand']
+        query_strand = self.wrappee['query_strand']
         assert type(query_strand) is str
         return query_strand
 
@@ -33,19 +33,19 @@ class HspWrapper:
 
     @property
     def hit_from(self):
-        hit_from = self.hsp['hit_from']
+        hit_from = self.wrappee['hit_from']
         assert type(hit_from) is int
         return hit_from
 
     @property
     def hit_to(self):
-        hit_to = self.hsp['hit_to']
+        hit_to = self.wrappee['hit_to']
         assert type(hit_to) is int
         return hit_to
 
     @property
     def hit_strand(self):
-        hit_strand = self.hsp['hit_strand']
+        hit_strand = self.wrappee['hit_strand']
         assert type(hit_strand) is str
         return hit_strand
 
